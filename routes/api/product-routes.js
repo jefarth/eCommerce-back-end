57ed18446c1695c4cb69ef5c94a3292ebf37b474
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-08-02 00:35:21 -0600

    updated product route page

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -118,7 +118,6 @@ router.delete('/:id', async (req, res) => {
       res.status(404).json({ message: 'No product found with this id!' });
       return;
     }
-
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
