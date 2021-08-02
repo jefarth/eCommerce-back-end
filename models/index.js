b6bfcc5fbfb75ad56dd10ce623e526d0da020fa3
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-08-01 21:23:20 -0600

    updated required paths

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const Category = require('./Category');
-const Product = require('/.Product');
-const ProductTag = require('/.ProductTag');
-const Tag = require('/.Tag');
+const Product = require('./Product');
+const ProductTag = require('./ProductTag');
+const Tag = require('./Tag');
 
 Category.hasMany(Product, {
     foreignKey: 'category_id',
